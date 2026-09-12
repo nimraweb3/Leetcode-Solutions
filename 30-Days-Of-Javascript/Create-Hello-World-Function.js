92151105bf6ec342546114c381b4088e177d9a0f
nimraweb3 <nimra.web3@gmail.com>
2026-09-12 22:20:18 +0500

    feat(30-Days-Of-Javascript): add counter solution

--- 30-Days-Of-Javascript/Create-Hello-World-Function.js
+++ 30-Days-Of-Javascript/Create-Hello-World-Function.js
@@ -1,6 +1,7 @@
-var createHelloWorld = function() {
-    
-    return function(...args) {
-        return "Hello World";
-    }
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+var createHelloWorld = function () {
+  return function (...args) {
+    return "Hello World";
+  };
 };
